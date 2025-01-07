9b458b7902bdbb7f0a6e34c13e50d1fda8d1f783
MairaKhan17 <mairakhan3377@gmail.com>
2025-01-07 22:52:33 +0500

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Featured from "@/components/Feature";
 import Hero from "@/components/Hero";
 import Menu from "@/components/Menu";
-import Navbar from "@/components/Navbar";
+// import Navbar from "@/components/Navbar";
 import Services from "@/components/Services";
 import Survey from "@/components/Survey";
 
